51967df4e1536d341f66dca970b04d303a282724
AdityaDutta128 <80472957+AdityaDutta128@users.noreply.github.com>
2021-06-06 11:02:06 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+
+var first_name = "Ranbir";
+console.log(first_name);
+
+var surname = "Kapoor";
+console.log(surname);
+
+var full_name = first_name.concat(surname);
+console.log(full_name);
+
+var a = 10;
+var b = 5;
+var add = a+b;
+console.log(add);
+
+var squarert = Math.sqrt(16);
+console.log(squarert);
